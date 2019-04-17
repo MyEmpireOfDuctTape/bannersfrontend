7f1bb7bfdc511f25bd49bd9fd7c25b529305c16a
Juhan Soone <jsoone@Juhans-iMac.local>
2019-04-17 14:59:47 +0300

    banner editing and template filtering

--- src/App.vue
+++ src/App.vue
@@ -24,8 +24,9 @@ export default {
 /* @import '../node_modules/vuetify/dist/vuetify.min.css'; */
 @import '../node_modules/@radial-color-picker/vue-color-picker/dist/vue-color-picker.min.css';  
 /* @import '../node_modules/vuetify/dist/vuetify.min.css'; */
-@import './build/app.min.css';
 @import './assets/css/animate.css';
+@import './build/app.min.css';
+
 </style>
 
 
